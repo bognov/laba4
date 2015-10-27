f29b585de9a939cfd8d7582af94dc669d0863a1d
Bogdan <nowikau@tut.by>
2015-10-27 12:03:30 +0300

    no message

---
+++ lab3/lab3/class.h
@@ -0,0 +1,102 @@
+#include <iostream>
+
+class vehicle
+{ 
+protected:
+	int length;
+	int width;
+	int hight;
+	int weight;
+	int maxspeed;
+public:
+	vehicle *next;
+	virtual void setsize(int, int, int);
+	virtual void setspeed(int);
+	virtual void setweight(int);
+	void getinfo();
+	friend class List;
+};
+
+class car :
+	public vehicle
+{
+public:
+	class engine
+	{
+	public:
+		int hp;
+		int volume;
+		engine(int a, int b)
+		{
+			sethp(a);
+			setvolume(b);
+		};
+		void sethp(int);
+		void setvolume(int);
+		void getinfo1();
+	};
+	car(int m[3], int a[2], int n)
+	{
+		setsize(m[0], m[1], m[2]);
+		setspeed(a[0]);
+		setweight(a[1]);
+		setplaces(n);
+	};
+	int numplaces;
+	void setplaces(int);
+	void getinfo(engine& m);
+};
+
+class train :
+	public vehicle
+{
+public:
+	class wagon
+	{
+	public:
+		int number;
+		int numplaces;
+		void setnumber(int);
+		void setplaces(int);
+		void getinfo1();
+	};
+	train(int m[3], int a[2], int n)
+	{
+		setsize(m[0], m[1], m[2]);
+		setspeed(a[0]);
+		setweight(a[1]);
+		setwagons(n);
+	};
+	int numwagons;
+	void getinfo(wagon& m);
+	void setwagons(int);
+};
+
+class express :
+	public vehicle
+{
+	int numplaces;
+public:
+	express(int m[3], int a[2], int n)
+	{
+		setsize(m[0], m[1], m[2]);
+		setspeed(a[0]);
+		setweight(a[1]);
+		setplaces(n);
+	};
+	void setplaces(int);
+	void getinfo();
+};
+
+class List 
+{ 
+public: List()
+{
+	head = 0; 
+}  
+~List();  
+void Show(void);
+void Insert(vehicle *);
+private:  
+	vehicle *head; 
+};  
---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,81 @@
+// lab3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "class.h"
+using namespace std;
+
+int setabc(int a[3])
+{
+	cout << "a ";
+	cin >> a[0];
+	cout << "b ";
+	cin >> a[1];
+	cout << "c ";
+	cin >> a[2];
+	return *a;
+}
+
+int setsm(int m[2])
+{
+	cout << "s ";
+	cin >> m[0];
+	cout << "m ";
+	cin >> m[1];
+	return *m;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a[3] , hp, n, m[2], v, w, p, i;
+	setabc(a);
+	setsm(m);
+	List L;
+	cout << "set places ";
+	cin >> n;
+	cout << "hp ";
+	cin >> hp;
+	cout << "volume ";
+	cin >> v;
+	car *A = new car(a, m, n);
+	car::engine *E = new car::engine(hp, v);
+
+	(*A).getinfo(*E);
+	L.Insert(A);
+
+	setabc(a);
+	setsm(m);
+	cout << "set wagons(<10) ";
+	cin >> w;
+	cout << "set places ";
+	cin >> p;
+	train *T = new train(a, m, w);
+
+	train::wagon W[10];
+	for (i = 0; i < w; i++)
+	{
+		W[i].setnumber(i+1);
+		W[i].setplaces(p);
+	}
+
+	cout << "number of wagon ";
+	cin >> i;
+
+	L.Insert(T);
+	(*T).getinfo(W[i-1]);
+
+	setabc(a);
+	setsm(m);
+	cout << "set places ";
+	cin >> n;
+	express *EX = new express(a, m, n);
+
+	(*EX).getinfo();
+	L.Insert(EX);
+
+	L.Show();
+
+	return 0;
+}
+
